e18d05f196e94311fc21a8572d8ca431d496d340
ttt <wfx>
2019-01-09 20:11:49 +0800

    six

--- fruit/src/views/detail.js
+++ fruit/src/views/detail.js
@@ -1,54 +1,152 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
 import axios from 'axios'
+import { PullToRefresh } from 'antd-mobile';
 import '../styles/detail.scss'
 import {getnew} from './something/target.js'
+import ReactDOM from "react-dom";
 class Detail extends Component{
     constructor (props){
         super(props)
         this.state={
             looplist:[],
             count:4,
+            refreshing: false,
+            down: true,
+            height: document.documentElement.clientHeight,
+            data:[],
+            num:1,
+            isloading:true
         }
     }
     componentDidMount(){
-        getnew(this.props.match.params.id,this.state.count).then(res=>{
+        const hei = this.state.height - ReactDOM.findDOMNode(this.ptr).offsetTop;
+        getnew(this.props.match.params.id,this.state.count,this.state.num).then(res=>{
             this.setState({
-                looplist:res
-            })
-        })
+                    height: hei,
+                   looplist:res
+               })
+               console.log(this.state.looplist)
+           }) 
     }
-
+    // componentDidMount() {
+    //     const hei = this.state.height - ReactDOM.findDOMNode(this.ptr).offsetTop;
+    //     setTimeout(() => this.setState({
+    //       height: hei,
+    //       data: ["1111","2222","3333"],
+    //     }), 0);
+    //   }
     render() {
-
+        
       return (
         <div id="detail">
-            <div>
+            <div id="loop">
                 <ul>
-                    <li>销量</li>
+                    <li onClick={this.changcount.bind(this,4)}>销量</li>
                     <li onClick={this.changplus.bind(this,5)}>新品</li>
-                    <li>价格</li>
+                    <li onClick={this.changprice.bind(this,1)}>价格</li>
                 </ul>
             </div>
-            <ul>
+            {/* <ul id="fic">
                 {
                     this.state.looplist.map((item,index)=>
-                        <li key={item.CommodityId}>
-                            <img src={item.SmallPic}/>{item.CommodityName}
+                        <li key={item.CommodityId} >
+                            <img src={item.SmallPic}/>
+                            <div>{item.CommodityName}<br/>
+                                    {item.SubTitle}<br/>
+                                    {item.PromotionTag}<br/>
+                                    ￥{item.SellPrice}  {item.Spec}    <button>+</button>
+                                    </div>
                         </li>
                     )
                 }
-            </ul>
+            </ul> */}
+                                      
+            <div id="box">
+                                <PullToRefresh
+                                    damping={60}
+                                    ref={el => this.ptr = el}
+                                    style={{
+                                    height: this.state.height-100,
+                                    overflow: 'auto',
+                                    }}
+                                    indicator={this.state.down ? {} : { deactivate: '上拉可以刷新' }}
+                                    direction={this.state.up ? 'down' : 'up'}
+                                    refreshing={this.state.refreshing}
+                                    onRefresh={() => {
+                                      
+                                    this.setState({ refreshing: true });
+
+                                    //axios...
+                                    // setTimeout(() => {
+                                    //     this.setState({ refreshing: false,
+                                    //         data:["kerwin","xiaoming",...this.state.data]
+                                    //     });
+                                    // }, 1000);
+                                    // if(this.state.looplist.length){
+                                    //     return 
+                                    // }
+                                        
+                                    if(this.state.isloading){
+                                        getnew(this.props.match.params.id,this.state.count,this.state.num+1).then(res=>{
+                                            this.setState({
+                                                   looplist:[...this.state.looplist,...res],
+                                                   num:this.state.num+1
+                                               })
+                                               if(res.length===0){
+                                                   this.setState({
+                                                       isloading:false
+                                                   })
+                                               }
+                                        })
+                                    }
+                                    }}
+                                >
+                        <ul id="fic">
+                                {
+                                    this.state.looplist.map((item,index)=>
+                                        <li key={item.CommodityId} >
+                                            <img src={item.SmallPic}/>
+                                            <div>{item.CommodityName}<br/>
+                                                    {item.SubTitle}<br/>
+                                                    {item.PromotionTag}<br/>
+                                                    ￥{item.SellPrice}  {item.Spec}    <button>+</button>
+                                                    </div>
+                                        </li>
+                                    )
+                                }
+                         </ul>
+                            </PullToRefresh>
+             </div>
         </div>
       )
     }
 
      
     changplus(el){
-        console.log(el)
-        this.setState({
-            count:el
-        })
+        getnew(this.props.match.params.id,this.state.count,this.state.num).then(res=>{
+            this.setState({
+                   looplist:res,
+                    count:el
+               })
+           })
+           
+    }
+    changcount(el){
+        getnew(this.props.match.params.id,el,this.state.num).then(res=>{
+            this.setState({
+                   looplist:res,
+                   count:el
+               })
+           })
+    }
+    changprice(el){
+        getnew(this.props.match.params.id,el,this.state.num).then(res=>{
+            this.setState({
+                   looplist:res,
+                   count:el
+               })
+         })
     }
 }
 
--- fruit/src/views/something/target.js
+++ fruit/src/views/something/target.js
@@ -14,14 +14,14 @@ function gettarget(){
       })
 }
 
-function getnew(id,index){
+function getnew(id,index,num){
     return axios({
         url:'https://b2capigateway.yiguo.com/api/commodityapi/Commodity/GetSearchList',
         method:'post',
         headers:{
             'appName': 3000025,
         },
-        data:{"Head":{"Token":"","LoginToken":"","CityId":"eabbe02f-59e0-46e6-90e7-cd8a89dbb98f","CityCode":"2","DistrictId":"751b5b8e-c1f7-4785-abeb-507b460f01ab","DeviceId":"34dc1400de48673cddd22a83a24c69e2","MobileOS":"Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"},"Body":{"Keyword":"","CategoryId":`${id}`,"CategoryCode":"","PageIndex":1,"Sort":`${index}`}}
+        data:{"Head":{"Token":"","LoginToken":"","CityId":"eabbe02f-59e0-46e6-90e7-cd8a89dbb98f","CityCode":"2","DistrictId":"751b5b8e-c1f7-4785-abeb-507b460f01ab","DeviceId":"34dc1400de48673cddd22a83a24c69e2","MobileOS":"Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"},"Body":{"Keyword":"","CategoryId":`${id}`,"CategoryCode":"","PageIndex":`${num}`,"Sort":`${index}`}}
     }).then(res=>{
         // console.log(res.data.Data.CommodityList)
         return res.data.Data.CommodityList
