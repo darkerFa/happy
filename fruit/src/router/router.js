c04e62c539df21eb9e83b3e009fecf6c7f27655b
sxm <1634251725@qq.com>
2019-01-09 09:19:24 +0800

    sxm修改router第三次

---
+++ fruit/src/router/router.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import {
+	HashRouter as Router,
+	Switch,
+	Route,
+  Redirect
+} from 'react-router-dom'
+import Footer from '../components/footer.js'
+import {Provider} from 'react-redux'
+import Eat from '../views/eat'
+import Home from '../views/home'
+import Sort from '../views/sort'
+import Center from '../views/center'
+import Shopping from '../views/shopping'
+import store from '../store/store'
+import Detail from '../views/detail'
+import City from '../components/city.js'
+
+class Routers extends Component {
+    render() {
+      return (
+        <Provider store={store}>
+            <Router>
+                <div>
+                    <Footer/>
+                    <Switch>
+                        <Route path='/home' component={Home}/>
+                        <Route path='/sort' component={Sort}/>
+                        <Route path='/eat' component={Eat}/>
+                        <Route path='/shopping' component={Shopping}/>
+                        <Route path='/center' component={Center}/>
+                        <Route path='/detail/:id' component={Detail}/>
+                        <Route path='/city' component={City}/>
+                        <Redirect from="*" to='/home'/>
+                    </Switch>
+                </div>
+            </Router>
+        </Provider>
+      )
+    }
+}
+
+export default  Routers
